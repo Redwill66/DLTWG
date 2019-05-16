f3535b1a7bef6261b3b03eae5b76393c61ef1c87
Daniel Trösch <48998656+Redwill66@users.noreply.github.com>
2019-05-16 09:47:38 +0200

    Nicht Spiel formulare

---
+++ Andere Seiten/NewplayerFunk.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameofLifedanielT
+{
+    class NewplayerFunk
+    {
+        public void User(string Name, string Surname, string Nummer)
+        {
+            DBfunk dbClass = new DBfunk();
+            using (var conn = dbClass.newMemb())
+            {
+
+
+                conn.Parameters.AddWithValue("@Name", Name);
+                conn.Parameters.AddWithValue("@Surname", Surname);
+
+
+
+                bool isUserExisted = UsernameCheck(Nummer);
+
+                if (isUserExisted)
+                {
+                    MessageBox.Show("MitgliedNr ist Vergeben");
+                }
+                else
+                {
+                    conn.Parameters.AddWithValue("@PlayerNr", Nummer);
+                    conn.ExecuteNonQuery();
+
+
+
+
+                }
+            }
+        }
+        public bool UsernameCheck(string Nummer)
+        {
+            bool isUserExisted = false;
+            DBfunk DBfunk = new DBfunk();
+            SqlCommand cmd = DBfunk.NumberCheck();
+            cmd.Parameters.AddWithValue("@PlayerNr", Nummer);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.HasRows == true)
+                {
+
+                    isUserExisted = true;
+                    break;
+                }
+
+            }
+            return isUserExisted;
+        }
+        public object[] Fore()
+        {
+            DBfunk dbClass = new DBfunk();
+            using (var conn = dbClass.LoadMem())
+            {
+
+                SqlDataReader reader;
+                reader = conn.ExecuteReader();
+
+                var Listt = new ListBox();
+                while (reader.Read())
+                {
+
+                    Listt.Items.Add(reader[3].ToString() + "   " + reader[1].ToString() + "     " + reader[2].ToString());
+
+
+                }
+                object[] objCollection = new object[Listt.Items.Count];
+                Listt.Items.CopyTo(objCollection, 0);
+                return objCollection;
+            }
+
+
+
+        }
+        public object[] Fore2()
+        {
+            DBfunk dbClass = new DBfunk();
+            using (var conn = dbClass.LoadMem())
+            {
+
+                SqlDataReader reader;
+                reader = conn.ExecuteReader();
+
+                var Listt = new ListBox();
+                while (reader.Read())
+                {
+
+                    Listt.Items.Add(reader[3].ToString() + "   " + reader[1].ToString() + "     " + reader[2].ToString());
+
+
+                }
+                object[] objCollection = new object[Listt.Items.Count];
+                Listt.Items.CopyTo(objCollection, 0);
+                return objCollection;
+            }
+
+
+
+        }
+        public object[] Fore3(string text)
+        {
+            DBfunk dbClass = new DBfunk();
+            using (var conn = dbClass.SerMembr())
+            {
+                conn.Parameters.AddWithValue("@PlayerNr", text);
+
+                SqlDataReader reader;
+                reader = conn.ExecuteReader();
+
+                var Listt = new ListBox();
+                while (reader.Read())
+                {
+
+                    Listt.Items.Add(reader[3].ToString() + "   " + reader[1].ToString() + "     " + reader[2].ToString());
+
+
+                }
+                object[] objCollection = new object[Listt.Items.Count];
+                Listt.Items.CopyTo(objCollection, 0);
+                return objCollection;
+            }
+
+
+
+        }
+        public ListViewItem[] Fore4()
+        {
+            DBfunk dbClass = new DBfunk();
+            using (var conn = dbClass.LoadMem())
+            {
+
+                SqlDataReader reader;
+                reader = conn.ExecuteReader();
+
+                var Listt = new ListBox();
+                while (reader.Read())
+                {
+
+                    Listt.Items.Add(reader[3].ToString() + "   " + reader[1].ToString() + "     " + reader[2].ToString());
+
+
+                }
+                ListViewItem[] objCollection = new ListViewItem[Listt.Items.Count];
+                Listt.Items.CopyTo(objCollection, 0);
+                return objCollection;
+            }
+
+
+
+        } 
+    }
+}
---
+++ Andere Seiten/Start.cs
@@ -0,0 +1,116 @@
+﻿using GameofLifedanielTextend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameofLifedanielT
+{
+    public partial class frmstart : Form
+    {
+        public frmstart()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGameoflifeO_Click(object sender, EventArgs e)
+        {
+            frmGameOfLife fnp = new frmGameOfLife();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnGameofLifeexperiment_Click(object sender, EventArgs e)
+        {
+            frmGameOfLifeex fnp = new frmGameOfLifeex();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnchess_Click(object sender, EventArgs e)
+        {
+            frmChess fnp = new frmChess();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnmine_Click(object sender, EventArgs e)
+        {
+            Minesweeper fnp = new Minesweeper();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnfourwin_Click(object sender, EventArgs e)
+        {
+            frmFourWin fnp = new frmFourWin();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnnewplayer_Click(object sender, EventArgs e)
+        {
+            frmNewPlayer fnp = new frmNewPlayer();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnFourLeaderboard_Click(object sender, EventArgs e)
+        {
+            frmRanglisteFourWin fnp = new frmRanglisteFourWin();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnrestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void btnGreed_Click(object sender, EventArgs e)
+        {
+            // Erinnerung an Daniel Brett mit 10 Farben Farbe 1 gibt 10 Punkte bei Klick plus 10 punkt für jedes Feld das Angrenzt und die gleiche Farbe hat. Klick auf die 10 Farbe eliminiert die Farben. Klick auf Farbe 1 macht es zu Farbe 2
+        }
+
+        private void btnsnake_Click(object sender, EventArgs e)
+        {
+            frmSnake fnp = new frmSnake();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnMemory_Click(object sender, EventArgs e)
+        {
+            Memory fnp = new Memory();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+
+        private void btnlaba_Click(object sender, EventArgs e)
+        {
+            Labyrinth fnp = new Labyrinth();
+            Hide();
+            fnp.ShowDialog();
+            Close();
+        }
+    }
+}
